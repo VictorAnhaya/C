6bae9dac4f863237f865d8dd9e9c0307856f4ad1
VictorAnhaya <victoranhaya8@icloud.com>
2025-03-31 20:07:37 -0300

    Teste

---
+++ Projeto.Victor.24/Program.cs
@@ -0,0 +1,15 @@
+﻿
+{
+    int numero = 10;
+    int numero_2 = 5;
+    Console.WriteLine("Quanto é: " + numero + "+" + numero_2 + "?");
+
+    int resposta = Convert.ToInt32(Console.ReadLine());
+
+    if (resposta == 15)
+    {
+        Console.Write("Parabéns ! Você acertou!");
+    }else{
+        Console.WriteLine("Tente novamente");
+    }
+}
